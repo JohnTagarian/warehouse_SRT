4f6a71286b9c362c5f809498ced10c65162ff3c2
Kantinan <pondkitnag@gmail.com>
2023-05-03 19:33:20 +0700

    pre maccanum

--- speed_control.h
+++ speed_control.h
@@ -1,7 +1,7 @@
-const int ENA[] = {5, 7, 4, 6};
-const int IN1[] = {49, 34, 46, 31};
-const int IN2[] = {48, 35, 47, 30};
-const int encoderPinA[] = {2, 19, 18, 3};
+const int ENA[] = {6, 5, 7, 4};
+const int IN1[] = {22, 28, 24, 26};
+const int IN2[] = {23, 29, 25, 27};
+const int encoderPinA[] = {19, 2, 18, 3};
 const int encoderPinB[] = {23, 25, 27, 29};
 
 // Define the encoder variables
@@ -11,10 +11,10 @@ volatile long encoderSpeed[] = {0, 0, 0, 0};
 unsigned long lastUpdateTime[] = {0, 0, 0, 0};
 
 // Define the PID variables
-const float Kp[] = {1, 1, 1, 1};
-const float Ki[] = {0.015, 0.015, 0.015, 0.015};
-const float Kd[] = {0.0025, 0.0025, 0.0025, 0.0025};
-const float targetRPM = 30;
+const float Kp[] = {2.0, 2.0, 2.0, 2.0};
+const float Ki[] = {0.025, 0.025, 0.025, 0.025};
+const float Kd[] = {0.001, 0.001, 0.001, 0.001};
+const float targetRPM = 35;
 
 float inputRPM[] = {0, 0, 0, 0};
 float outputPWM[] = {0, 0, 0, 0};
@@ -29,15 +29,12 @@ unsigned long init_time;
 
 
 // compute speed control
-void compute_pid_motor(int index, int target , int dir) {
-
-  if (!check_time_init) {
-    init_time = millis();
-    check_time_init = true;
-  }
+void compute_pid_motor(int index, int target , int dir, bool enable) {
 
   unsigned long currentTime = millis() - init_time; // Line 36
-  unsigned long deltaTime = currentTime - lastUpdateTime[index]; // Line 37
+  unsigned long deltaTime;
+  if (!enable) deltaTime = 0;
+  else  deltaTime = currentTime - lastUpdateTime[index]; // Line 37
   lastUpdateTime[index] = currentTime; // Line 38
 
   long countDiff = encoderCount[index] - lastEncoderCount[index]; // Line 40
@@ -58,56 +55,60 @@ void compute_pid_motor(int index, int target , int dir) {
   outputPWM[index] = constrain(outputPWM[index], 0, 255); // Line 56
 
 
-  if (dir == 1) {
+  if (dir == -1) {
     digitalWrite(IN1[index], HIGH);
     digitalWrite(IN2[index], LOW);
     analogWrite(ENA[index], outputPWM[index]);
   }
-  else if (dir == -1)
+  else if (dir == 1)
   {
     digitalWrite(IN1[index], LOW);
     digitalWrite(IN2[index], HIGH);
     analogWrite(ENA[index], outputPWM[index]);
   }
-  else {
+  else if (dir == 0 ) {
     digitalWrite(IN1[index], LOW);
     digitalWrite(IN2[index], LOW);
     analogWrite(ENA[index], 0);
   }
 
-  //  Serial.print(index);
-  //  Serial.print("\tSpeed : ");
-  //  Serial.print(encoderSpeed[index]);
-  //  Serial.print("\tCount :");
-  //  Serial.print(encoderCount[index]);
-  //  Serial.print("\tcountdiff : ");
-  //  Serial.print(countDiff);
-  //  Serial.print("\tDT : ");
-  //  Serial.print(deltaTime);
-  //  Serial.print("\tError : ");
-  //  Serial.println(error);
+
+//    Serial.print(target);
+//    Serial.print(",");
+//    Serial.println(encoderSpeed[index]);
+//    Serial.print(index);
+//    Serial.print("\tSpeed : ");
+//    Serial.print(encoderSpeed[index]);
+//    Serial.print("\tCount :");
+//    Serial.print(encoderCount[index]);
+//    Serial.print("\tcountdiff : ");
+//    Serial.print(countDiff);
+//    Serial.print("\tDT : ");
+//    Serial.print(deltaTime);
+//    Serial.print("\tError : ");
+//    Serial.println(error);
 
 }
 
 
 // ISR section
 void updateEncoder_Q() {
-  dir[0] = digitalRead(encoderPinB[0]);
+  //  dir[0] = digitalRead(encoderPinB[0]);
   encoderCount[0]++;
 }
 
 void updateEncoder_E()
 {
-  dir[1] = digitalRead(encoderPinB[1]);
+  //  dir[1] = digitalRead(encoderPinB[1]);
   encoderCount[1]++;
 }
 void updateEncoder_A()
 {
-  dir[2] = digitalRead(encoderPinB[2]);
+  //  dir[2] = digitalRead(encoderPinB[2]);
   encoderCount[2]++;
 }
 void updateEncoder_D()
 {
-  dir[3] = digitalRead(encoderPinB[3]);
+  //  dir[3] = digitalRead(encoderPinB[3]);
   encoderCount[3]++;
 }
--- warehouse_SRT.ino
+++ warehouse_SRT.ino
@@ -1,18 +1,18 @@
-#include <MCP3008.h>
-#include <SPI.h>
-#include "IMU.h"
+//#include <MCP3008.h>
+//#include <SPI.h>
+//#include "IMU.h"
 #include "speed_control.h"
 
-#define CS_PIN 12
-#define CLOCK_PIN 9
-#define MOSI_PIN 11
-#define MISO_PIN 10
+//#define CS_PIN 12
+//#define CLOCK_PIN 9
+//#define MOSI_PIN 11
+//#define MISO_PIN 10
 
-MCP3008 adc(CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN);
+//MCP3008 adc(CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN);
 
 
-
-int irValue[8];
+int track_ir[] = {11, 10, 9, 8};
+int track_val[] = {0, 0, 0, 0};
 
 const float trck_kp = 1.50;
 const float trck_kd = 0.05;
@@ -23,12 +23,13 @@ float last_trck_err = 0;
 float track_errorSum = 0;
 
 const int numSensors = 8;
-int sensorThreshold = 60;
 
 int base_speed = 100;
 int max_speed = 200;
+int last_track_state = 0;
 
-
+int thredhold = 60;
+int speed_track = 45;
 void setup() {
   Serial.begin(115200);
   while (!Serial); // wait for Leonardo enumeration, others continue immediately
@@ -39,6 +40,7 @@ void setup() {
     pinMode(ENA[i], OUTPUT);
     pinMode(IN1[i], OUTPUT);
     pinMode(IN2[i], OUTPUT);
+    pinMode(track_ir, INPUT_PULLUP);
 
     // Initialize the encoder pins as input
     pinMode(encoderPinA[i], INPUT_PULLUP); // Line 25
@@ -49,135 +51,281 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(encoderPinA[2]), updateEncoder_A, RISING);
   attachInterrupt(digitalPinToInterrupt(encoderPinA[3]), updateEncoder_D, RISING);
 
-
   Serial.println(F("Start:"));
-  SPI.begin();
-  SPI.setClockDivider(SPI_CLOCK_DIV16); // set SPI clock to 1MHz
-  SPI.setDataMode(SPI_MODE0);
-  SPI.setBitOrder(MSBFIRST);
 
 
 
   // Setup the MPU and TwoWire aka Wire library all at once
 
-  mpu.begin();
-  mpu.Set_DMP_Output_Rate_Hz(100);          // Set the DMP output rate from 200Hz to 5 Minutes.
+  //  mpu.begin();
+  //  mpu.Set_DMP_Output_Rate_Hz(100);          // Set the DMP output rate from 200Hz to 5 Minutes.
   //mpu.Set_DMP_Output_Rate_Seconds(10);   // Set the DMP output rate in Seconds
   //mpu.Set_DMP_Output_Rate_Minutes(5);    // Set the DMP output rate in Minute
-  mpu.CalibrateMPU();                      // Calibrates the MPU.
-  mpu.load_DMP_Image();                    // Loads the DMP image into the MPU and finish configuration.
-  mpu.on_FIFO(Print_Values);               // Set callback function that is triggered when FIFO Data is retrieved
+  //  mpu.CalibrateMPU();                      // Calibrates the MPU.
+  //  mpu.load_DMP_Image();                    // Loads the DMP image into the MPU and finish configuration.
+  //  mpu.on_FIFO(Print_Values);               // Set callback function that is triggered when FIFO Data is retrieved
   // Setup is complete!
 
 }
-
+unsigned long pretime;
+char state = '1';
 void loop() {
 
-  //  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
-  //  irValue[0] = adc.readADC(0) / 10;
-  //  irValue[1] = adc.readADC(1) / 10;
-  //  irValue[2] = adc.readADC(2) / 10 - 10;
-  //  irValue[3] = adc.readADC(3) / 10 + 10;
-  //  irValue[4] = adc.readADC(4) / 10;
-  //  irValue[5] = adc.readADC(5) / 10;
-  //  irValue[6] = adc.readADC(6) / 10;
-  //  irValue[7] = adc.readADC(7) / 10;
-  //  SPI.endTransaction();
-  //  for (int i = 0; i < 8; i++) {
-  //    Serial.print(irValue[i]);
-  //    Serial.print("\t");
+  //  if (millis() - pretime > 3500) {
+  //    pretime = millis();
+  //    state = !state;
+  //
+  //  }
+  //  if (state) {
+  //    compute_pid_motor(0, 40, 1, true);
+  //    compute_pid_motor(1, 40, -1, true);
+  //    compute_pid_motor(2, 40, -1, true);
+  //    compute_pid_motor(3, 40, 1, true);
+  //  }
+  //  else {
+  //    wait_speed_control();
   //  }
 
-  compute_pid_heading();
+  //    compute_pid_motor(0, 40, 1, true);
+  //    compute_pid_motor(1, 10, -1, true);
+  //    compute_pid_motor(2, 40, 1, true);
+  //    compute_pid_motor(3, 10, -1, true);
+
+
+  //  compute_pid_heading();
+
+
+  for (int i = 0 ; i < 4 ; i++ ) {
+    if (i == 2)track_val[i] = analogRead(track_ir[i]) / 10 + 8;
+    else track_val[i] = analogRead(track_ir[i]) / 10;
+  }
+
+  Serial.print(String(track_val[1]) + "," + String(track_val[1]) + "," + String(track_val[2]) + "," + String(track_val[3]));
+  
+//  compute_pid_motor(0, 45, 1, true);
+//  compute_pid_motor(1, 45, -1, true);
+//  compute_pid_motor(2, 45, 1, true);
+//  compute_pid_motor(3, 45, -1, true);
+  //
+  //  switch (state) {
+  //
+  //    case '1':
+  //      while (!((track_val[0] < thredhold && track_val[1]  < thredhold && track_val[2] < thredhold && track_val[3] - 5 < thredhold))) {
+  //        Serial.println("state 1");
+  //        compute_line_track();
+  //      }
+  //      pretime = millis();
+  //      while (millis() - pretime < 1200) {
+  //        compute_pid_motor(0, 25, 1, true);
+  //        compute_pid_motor(1, 25, 1, true);
+  //        compute_pid_motor(2, 25, 1, true);
+  //        compute_pid_motor(3, 25, 1, true);
+  //      }
+  //
+  //      pretime = millis();
+  //      while (millis() - pretime < 3000) {
+  //        compute_pid_motor(0, 45, 1, true);
+  //        compute_pid_motor(1, 45, -1, true);
+  //        compute_pid_motor(2, 45, 1, true);
+  //        compute_pid_motor(3, 45, -1, true);
+  //      }
+  //      state = '2';
+  //      break;
+  //    case '2':
+  //      Serial.println("state 2");
+  //      wait_speed_control();
+  //
+  //      break;
+  //
+  //
+  //  }
 }
 
 
 // pid line track
-void compute_pid_line_track() {
+void compute_line_track() {
+  for (int i = 0 ; i < 4 ; i++ ) {
+    if (i == 2)track_val[i] = analogRead(track_ir[i]) / 10 + 8;
+    else track_val[i] = analogRead(track_ir[i]) / 10;
+  }
 
-  // Calculate the error
-  trck_error = 0;
-  for (int i = 0; i < numSensors; i++) {
-    if (irValue[i] < sensorThreshold) {
-      trck_error += (i - (numSensors / 2)) * (sensorThreshold - irValue[i] );
+  Serial.print(String(track_val[1]) + "," + String(track_val[1]) + "," + String(track_val[2]) + "," + String(track_val[3]));
+  if (track_val[0] < thredhold && track_val[1]  < thredhold && track_val[2] < thredhold && track_val[3] - 5 < thredhold) {
+    Serial.println("Find state");
+    //    compute_pid_motor(0, 0, 0, false);
+    //    compute_pid_motor(1, 0, 0, false);
+    //    compute_pid_motor(2, 0, 0, false);
+    //    compute_pid_motor(3, 0, 0, false);
+  }
+  else if (!(track_val[0] < thredhold) && !(track_val[1]  < thredhold) && !(track_val[2] < thredhold) && !(track_val[3] < thredhold)) {
+    Serial.println("clean");
+    compute_pid_motor(0, speed_track, 1, true);
+    compute_pid_motor(1, speed_track, 1, true);
+    compute_pid_motor(2, speed_track, 1, true);
+    compute_pid_motor(3, speed_track, 1, true);
+
+  }
+  else if (track_val[0] < thredhold && !(track_val[1]  < thredhold) && !(track_val[2] < thredhold) && !(track_val[3] < thredhold)) {
+    Serial.println("overright error");
+    last_track_state = 1;
+    compute_pid_motor(0, speed_track - 25, -1, true);
+    compute_pid_motor(1, speed_track, 1, true);
+    compute_pid_motor(2, speed_track - 25, -1, true);
+    compute_pid_motor(3, speed_track, 1, true);
+  }
+  else if (track_val[0] < thredhold && track_val[1]  < thredhold && !(track_val[2] < thredhold) && !(track_val[3] < thredhold)) {
+    Serial.println("farly right error");
+    last_track_state = 2;
+    compute_pid_motor(0, speed_track - 25, -1, true);
+    compute_pid_motor(1, speed_track, 1, true);
+    compute_pid_motor(2, speed_track - 25, -1, true);
+    compute_pid_motor(3, speed_track, 1, true);
+  }
+  else if (!(track_val[0] < thredhold) && track_val[1]  < thredhold && !(track_val[2] < thredhold) && !(track_val[3] < thredhold)) {
+    Serial.println("right error");
+    last_track_state = 3;
+    compute_pid_motor(0, 0, 0, false);
+    compute_pid_motor(1, speed_track, 1, true);
+    compute_pid_motor(2, 0, 0, false);
+    compute_pid_motor(3, speed_track, 1, true);
+  }
+
+  else if (!(track_val[0] < thredhold) && !(track_val[1] < thredhold ) && !(track_val[2] < thredhold ) && track_val[3] < thredhold) {
+    Serial.println("overleft erorr");
+    last_track_state = 4;
+    compute_pid_motor(0, speed_track, 1, true);
+    compute_pid_motor(1, speed_track - 25, -1, true);
+    compute_pid_motor(2, speed_track, 1, true);
+    compute_pid_motor(3, speed_track - 25, -1, true);
+  }
+  else if (!(track_val[0] < thredhold ) && !(track_val[1]  < thredhold ) && track_val[2] < thredhold && track_val[3] < thredhold) {
+    Serial.println("farly left error");
+    last_track_state = 5;
+    compute_pid_motor(0, speed_track, 1, true);
+    compute_pid_motor(1, speed_track - 25, -1, true);
+    compute_pid_motor(2, speed_track, 1, true);
+    compute_pid_motor(3, speed_track - 25, -1, true);
+  }
+  else if (!(track_val[0] < thredhold) && !(track_val[1]  < thredhold ) && track_val[2] < thredhold && !(track_val[3] < thredhold)) {
+    Serial.println("left error");
+    last_track_state = 6;
+    compute_pid_motor(0, speed_track, 1, true);
+    compute_pid_motor(1, 0, 0, false);
+    compute_pid_motor(2, speed_track, 1, true);
+    compute_pid_motor(3, 0, 0, false);
+  }
+  else {
+    Serial.println("Else");
+    if (last_track_state == 1) {
+      compute_pid_motor(0, speed_track - 25, -1, true);
+      compute_pid_motor(1, speed_track, 1, true);
+      compute_pid_motor(2, speed_track - 25, -1, true);
+      compute_pid_motor(3, speed_track, 1, true);
+    }
+    else if (last_track_state == 2) {
+      compute_pid_motor(0, speed_track - 25, -1, true);
+      compute_pid_motor(1, speed_track, 1, true);
+      compute_pid_motor(2, speed_track - 25, -1, true);
+      compute_pid_motor(3, speed_track, 1, true);
+    }
+    else if (last_track_state == 3) {
+      compute_pid_motor(0, 0, 0, false);
+      compute_pid_motor(1, speed_track, 1, true);
+      compute_pid_motor(2, 0, 0, false);
+      compute_pid_motor(3, speed_track, 1, true);
+    }
+    else if (last_track_state == 4) {
+      compute_pid_motor(0, speed_track, 1, true);
+      compute_pid_motor(1, speed_track - 25, -1, true);
+      compute_pid_motor(2, speed_track, 1, true);
+      compute_pid_motor(3, speed_track - 25, -1, true);
+    }
+    else if (last_track_state == 5) {
+      compute_pid_motor(0, speed_track, 1, true);
+      compute_pid_motor(1, speed_track - 25, -1, true);
+      compute_pid_motor(2, speed_track, 1, true);
+      compute_pid_motor(3, speed_track - 25, -1, true);
 
     }
+    else if (last_track_state == 6) {
+      last_track_state = 6;
+      compute_pid_motor(0, speed_track, 1, true);
+      compute_pid_motor(1, 0, 0, false);
+      compute_pid_motor(2, speed_track, 1, true);
+      compute_pid_motor(3, 0, 0, false);
+    }
+    //    compute_pid_motor(0, 30, 1);
+    //    compute_pid_motor(1, 30, 1);
+    //    compute_pid_motor(2, 30, 1);
+    //    compute_pid_motor(3, 30, 1);
   }
-  Serial.print(" Error :");
-  Serial.print(trck_error);
+  Serial.println();
 
-  float track_derivative = trck_error - last_trck_err;
-  track_errorSum += trck_error;
-
-  float track_output = (trck_kp * trck_error) + (trck_kd * track_derivative) + (trck_ki * track_errorSum);
-
-
-  //  analogWrite(ENA, constrain(base_speed - track_output, 0, max_speed));
-  //  analogWrite(ENB, constrain(base_speed + track_output, 0, max_speed));
-
-
-  last_trck_err = trck_error;
 }
 
 
-
+void wait_speed_control() {
+  for (int i = 0; i < 4 ; i++) {
+    compute_pid_motor(i , 0, 1, false);
+  }
+}
 // pid heading control
-void compute_pid_heading() {
-  static unsigned long FIFO_DelayTimer;
-  if ((millis() - FIFO_DelayTimer) >= (99)) { // 99ms instead of 100ms to start polling the MPU 1ms prior to data arriving.
-    if ( mpu.dmp_read_fifo(false)) FIFO_DelayTimer = millis() ; // false = no interrupt pin attachment required and When data arrives in the FIFO Buffer reset the timer
-  }
-
-  IMU_error = IMU_setpoint - yaw_value;
-
-  if (IMU_error < - 180) {
-    IMU_error += 360;
-  }
-  if (IMU_error > 180) {
-    IMU_error -= 360;
-  }
-
-  // Calculate the error sum and difference
-  IMU_error_sum += IMU_error;
-  IMU_error_diff = IMU_error - IMU_last_error;
-
-  // Calculate the PID output
-  IMU_output = IMU_Kp * IMU_error + IMU_Ki * IMU_error_sum * IMU_dt + IMU_Kd * IMU_error_diff / IMU_dt;
-
-  IMU_output = constrain(IMU_output, -150, 150);
-  heading_speed = abs(IMU_output);
-  heading_speed = constrain(heading_speed, 0, 60);
-
-
-
-  if (IMU_output < 0) {
-
-    compute_pid_motor(0, heading_speed, -1);
-    compute_pid_motor(1, heading_speed, 1);
-    compute_pid_motor(2, heading_speed, -1);
-    compute_pid_motor(3, heading_speed, 1);
-  } else {
-
-    compute_pid_motor(0, heading_speed, 1);
-    compute_pid_motor(1, heading_speed, -1);
-    compute_pid_motor(2, heading_speed, 1);
-    compute_pid_motor(3, heading_speed, -1);
-  }
-
-
-  Serial.print("ERR : ");
-  Serial.print(IMU_error);
-  Serial.print("\tIMU_output :");
-  Serial.print(IMU_output);
-  Serial.print("\tYaw : ");
-  Serial.println(yaw_value);
-
-  IMU_last_error = IMU_error;
-
-  // Calculate the current time and dt
-  IMU_current_time = millis();
-  IMU_dt = (IMU_current_time - IMU_last_time) / 1000.0;
-
-  // Store the current time as the last time
-  IMU_last_time = IMU_current_time;
-}
+//void compute_pid_heading() {
+//  static unsigned long FIFO_DelayTimer;
+//  if ((millis() - FIFO_DelayTimer) >= (99)) { // 99ms instead of 100ms to start polling the MPU 1ms prior to data arriving.
+//    if ( mpu.dmp_read_fifo(false)) FIFO_DelayTimer = millis() ; // false = no interrupt pin attachment required and When data arrives in the FIFO Buffer reset the timer
+//  }
+//
+//  IMU_error = IMU_setpoint - yaw_value;
+//
+//  if (IMU_error < - 180) {
+//    IMU_error += 360;
+//  }
+//  if (IMU_error > 180) {
+//    IMU_error -= 360;
+//  }
+//
+//  // Calculate the error sum and difference
+//  IMU_error_sum += IMU_error;
+//  IMU_error_diff = IMU_error - IMU_last_error;
+//
+//  // Calculate the PID output
+//  IMU_output = IMU_Kp * IMU_error + IMU_Ki * IMU_error_sum * IMU_dt + IMU_Kd * IMU_error_diff / IMU_dt;
+//
+//  IMU_output = constrain(IMU_output, -150, 150);
+//  heading_speed = abs(IMU_output);
+//  heading_speed = constrain(heading_speed, 0, 60);
+//
+//
+//
+//  if (IMU_output < 0) {
+//
+//    compute_pid_motor(0, heading_speed, -1);
+//    compute_pid_motor(1, heading_speed, 1);
+//    compute_pid_motor(2, heading_speed, -1);
+//    compute_pid_motor(3, heading_speed, 1);
+//  } else {
+//
+//    compute_pid_motor(0, heading_speed, 1);
+//    compute_pid_motor(1, heading_speed, -1);
+//    compute_pid_motor(2, heading_speed, 1);
+//    compute_pid_motor(3, heading_speed, -1);
+//  }
+//
+//
+//  Serial.print("ERR : ");
+//  Serial.print(IMU_error);
+//  Serial.print("\tIMU_output :");
+//  Serial.print(IMU_output);
+//  Serial.print("\tYaw : ");
+//  Serial.println(yaw_value);
+//
+//  IMU_last_error = IMU_error;
+//
+//  // Calculate the current time and dt
+//  IMU_current_time = millis();
+//  IMU_dt = (IMU_current_time - IMU_last_time) / 1000.0;
+//
+//  // Store the current time as the last time
+//  IMU_last_time = IMU_current_time;
+//}
